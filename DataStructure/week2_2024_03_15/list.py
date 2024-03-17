13e278679d2ff44c81f3fc8615aa7d43d8973a51
SongSeungYun <94339418+seungyun0816@users.noreply.github.com>
2024-03-17 22:11:02 +0900

    Add files via upload

---
+++ DataStructure/week2_2024_03_15/list.py
@@ -0,0 +1,48 @@
+fruits = ["apple", "kiwi", "mango"]
+m = ["0",   1,2]
+for item in fruits:
+    print(item)
+
+print("==============")
+
+#연습내용
+
+for i in fruits[0]:
+    print(i,end="")
+
+print("")
+
+for i in fruits[0]:
+    print(i)
+
+print("============")
+
+for i in fruits[1]:
+    print(i,end="")
+
+print("")
+
+for i in fruits[1]:
+    print(i)
+
+print("============")
+
+for i in fruits[2]:
+    print(i,end="")
+
+print("")
+
+for i in fruits[2]:
+    print(i)
+
+print("============")
+
+#여러줄 문자열 """로 시작 """로 끝내기
+#변수에 저장 가능
+
+intro = """
+동의과학대
+컴소과
+승윤
+"""
+print (intro)
